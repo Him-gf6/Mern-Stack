7897e7a3b51f4a36f286d68cfbec0e150172f79d
Himadri Sarkar <himadrisarkar844@gmail.com>
2025-12-31 21:02:58 +0530

    changes

--- ZOMATO/Backend/server.js
+++ ZOMATO/Backend/server.js
@@ -4,7 +4,9 @@ const connectDB=require('./src/db/db.js');
 
 connectDB();
 
-app.listen(process.env.PORT || 3000,()=>{
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
     console.log("Server is running on port 3000");
 })
 
