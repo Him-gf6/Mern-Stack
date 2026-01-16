b62cd74075092bb0fb830388add0ce0b9ace39de
Himadri Sarkar <himadrisarkar844@gmail.com>
2026-01-16 13:39:01 +0530

    committed

--- ZOMATO/Backend/src/controllers/auth.controller.js
+++ ZOMATO/Backend/src/controllers/auth.controller.js
@@ -6,10 +6,6 @@ const bcrypt=require('bcryptjs');
 const registerUser=async (req,res)=>{
    const {name,email,password}=req.body;
 
-   if (!name || !email || !password) {
-      return res.status(400).json({ message: "All fields are required" });
-    }
-
    const existingUser=await userModel.findOne({email:email});
    if(existingUser){
        return res.status(400).json({message:"User already exists"});
